e7fcbdc787712a5a04c36eb2eee1300d7f87ef06
Archit Soni <69715914+ArchitSoni07@users.noreply.github.com>
2021-08-01 15:19:24 +0530

    Add files via upload

---
+++ Algorithms and codes/LinkedList/Find_Loop.py
@@ -0,0 +1,14 @@
+# SINGELY LINKED LIST
+def findLoop(head):
+    first = head.next
+    second = head.next.next
+    while first != second:
+        first = first.next
+        second = second.next.next
+    first = head
+    while first != second:
+        first = first.next
+        second = second.next
+    return first
+
+# Time-O(n) ||| space-O(1)
