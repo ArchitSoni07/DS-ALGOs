b66b34c279e11c34d56a852f86e04cbe6cef1d5e
Archit Soni <69715914+ArchitSoni07@users.noreply.github.com>
2021-11-26 11:23:07 +0530

    Update Hill_Cipher.py

--- Algorithms and codes/ciphers/Hill_Cipher.py
+++ Algorithms and codes/ciphers/Hill_Cipher.py
@@ -1,8 +1,7 @@
 #Hill Cipher
+import numpy as np
 keyMatrix = [[0] * 3 for i in range(3)]
-
 messageVector = [[0] for i in range(3)]
-
 cipherMatrix = [[0] for i in range(3)]
 
 def getKeyMatrix(key):
@@ -12,6 +11,14 @@ def getKeyMatrix(key):
 			keyMatrix[i][j] = ord(key[k]) % 65
 			k += 1
 
+def getInvertedKeyMatrix(key):
+    k = 0
+	for i in range(3):
+		for j in range(3):
+			keyMatrix[i][j] = ord(key[k]) % 65
+			k += 1
+    invertedKeyMatrix = np.linalg.inv(keyMatrix)
+
 def encrypt(messageVector):
 	for i in range(3):
 		for j in range(1):
@@ -20,21 +27,29 @@ def encrypt(messageVector):
 				cipherMatrix[i][j] += (keyMatrix[i][x] *
 									messageVector[x][j])
 			cipherMatrix[i][j] = cipherMatrix[i][j] % 26
+			
+def decrypt(invertedKeyMatrix):
+    text = []
+    for i in invertedKeyMatrix:
+        text.append(chr(chr(i)-65))
+    return text
 
 def HillCipher(message, key):
 
+	getInvertedKeyMatrix(key)
 	getKeyMatrix(key)
-
+	
 	for i in range(3):
 		messageVector[i][0] = ord(message[i]) % 65
 
 	encrypt(messageVector)
-
 	CipherText = []
 	for i in range(3):
 		CipherText.append(chr(cipherMatrix[i][0] + 65))
 
 	print("Ciphertext: ", "".join(CipherText))
+    print(decrypt(invertedKeyMatrix))
+
 
 def main():
 
